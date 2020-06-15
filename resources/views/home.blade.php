3d56b44c13a2b486b48185d110ccd39b8ed58d40
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-15 10:25:24 +0200

    added home route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <section class="link-box">
+        <h2>Main content</h2>
+        <a href="{{ route('home') }}">Home</a>
+        <a href="">Blog</a>
+    </section>
+@endsection
